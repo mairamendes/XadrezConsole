f5d6ae267f487743061d88cc25686ece0ba1e33d
Maíra Mendes <maira.silva@groupsoftware.com.br>
2025-06-27 10:13:29 -0300

    Criando  a classe Posicao

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml;
+using tabuleiro;
+
+namespace JogoDeXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
+        }
+
+      
+    }
+}
